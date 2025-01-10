9ef90690406be1d606d29aba9cbd5b6d95f6818b
hitorihellen <leviettungduong@gmail.com>
2025-01-10 20:51:07 +0700

    modify tlp

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,6 +25,33 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  programs.nh = {
+    enable = true;
+    flake = "/home/linux/nix-config";
+  };
+  # tlp setting
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+  enable = true;
+  settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+    # CPU_MIN_PERF_ON_AC = 0;
+    # CPU_MAX_PERF_ON_AC = 100;
+    # CPU_MIN_PERF_ON_BAT = 0;
+    # CPU_MAX_PERF_ON_BAT = 20;
+
+    # # Optional helps save long term battery health
+    # START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+    # STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+  };
+};
+
+
   # Set your time zone.
   time.timeZone = "Asia/Ho_Chi_Minh";
 
